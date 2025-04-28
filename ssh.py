0e0a79cfaf65a37a3d6d354f478ca47679cb8838
richu <1691401076@qq.com>
2025-04-28 22:04:21 +0800

    fixed:fixed ssh connect

--- ssh.py
+++ ssh.py
@@ -2,43 +2,47 @@ import os
 
 
 class config:
-    def __init__(self, name, email):
+    def __init__(self, name, email,ssh_config_name="id_rsa"):
         self.name = name
         self.email = email
+        self.ssh_config_name = ssh_config_name
+        username = os.getlogin()
+        self.config_path = f"C:\\Users\\{username}\\.ssh\\config"
+
+    def setUser(self):
+        os.system(f'git config --global user.email "{self.email}"')
+        os.system(f'git config --global user.name "{self.name}"')
+        with open(self.config_path, "w") as f:
+            f.write("Host github.com\n")
+            f.write(f"\tIdentityFile ~/.ssh/{self.ssh_config_name}")
+
+    def showUser(self):
+        print("git global UserName", end=": ")
+        os.system('git config --global user.name')
+        print("git global UserEmail", end=": ")
+        os.system('git config --global user.email')
+        print("\ncurrent ssh\config: ")
+        with open(self.config_path, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                print(line, end="")
 
 
-plana = config("plana", "13058160568@163.com")
+plana = config("plana", "13058160568@163.com","plana_id_rsa")
 richu = config("richu", "1691401076@qq.com")
 
-
-def setUser(config, config_path):
-    os.system(f'git config --global user.email "{config.email}"')
-    os.system(f'git config --global user.name "{config.name}"')
-    with open(config_path, "w") as f:
-        f.write("Host github.com\n")
-        if config.name == "plana":
-            f.write(f"\tIdentityFile ~/.ssh/{config.name}_id_rsa")
-        elif config.name == "richu":
-            f.write(f"\tIdentityFile ~/.ssh/id_rsa")
-
-
-def showUser():
-    print("git global UserName", end=": ")
-    os.system('git config --global user.name')
-    print("git global UserEmail", end=": ")
-    os.system('git config --global user.email')
-    print("\ncurrent ssh\config: ")
-    with open(config_path, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            print(line, end="")
-
-
 if __name__ == '__main__':
-    username = os.getlogin()
-    config_path = f"C:\\Users\\{username}\\.ssh\\config"
-    setUser(richu, config_path)
-    showUser()
+    # plana.setUser()
+    # plana.showUser()
+    richu.setUser()
+    richu.showUser()
+
+
+
+
+
+
+
 
 # with open(config_path, "w+r") as f:
 #     line = f.readline()
