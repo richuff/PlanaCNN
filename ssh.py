210565885c2a55d6ed9fcca435d494aad65a61c8
richu <1691401076@qq.com>
2025-04-28 21:52:50 +0800

    fixed:fixed ssh connect

--- ssh.py
+++ ssh.py
@@ -10,19 +10,11 @@ class config:
 plana = config("plana", "13058160568@163.com")
 richu = config("richu", "1691401076@qq.com")
 
-# with open(config_path, "w+r") as f:
-#     line = f.readline()
-#     while line:
-#         if "IdentityFile ~/.ssh/id_rsa" in line and config.name == "plana":
-#             f.write(f"IdentityFile ~/.ssh/{config.name}_id_rsa")
-#         elif "IdentityFile" in line and config.name == "richu":
-#             f.write(f"IdentityFile ~/.ssh/id_rsa")
-#         f.write(line)
-#         line = f.readline()
+
 def setUser(config, config_path):
     os.system(f'git config --global user.email "{config.email}"')
     os.system(f'git config --global user.name "{config.name}"')
-    with open(config_path,"w") as f:
+    with open(config_path, "w") as f:
         f.write("Host github.com\n")
         if config.name == "plana":
             f.write(f"\tIdentityFile ~/.ssh/{config.name}_id_rsa")
@@ -35,6 +27,11 @@ def showUser():
     os.system('git config --global user.name')
     print("git global UserEmail", end=": ")
     os.system('git config --global user.email')
+    print("\ncurrent ssh\config: ")
+    with open(config_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line, end="")
 
 
 if __name__ == '__main__':
@@ -42,3 +39,13 @@ if __name__ == '__main__':
     config_path = f"C:\\Users\\{username}\\.ssh\\config"
     setUser(richu, config_path)
     showUser()
+
+# with open(config_path, "w+r") as f:
+#     line = f.readline()
+#     while line:
+#         if "IdentityFile ~/.ssh/id_rsa" in line and config.name == "plana":
+#             f.write(f"IdentityFile ~/.ssh/{config.name}_id_rsa")
+#         elif "IdentityFile" in line and config.name == "richu":
+#             f.write(f"IdentityFile ~/.ssh/id_rsa")
+#         f.write(line)
+#         line = f.readline()
